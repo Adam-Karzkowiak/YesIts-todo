f90f53d182a66a95e0c0a586bd6d5c03a5b5ca06
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-17 16:47:31 +0100

    Getters and setters added (annotation)

--- src/main/java/com/app/YesItstodo/model/Task.java
+++ src/main/java/com/app/YesItstodo/model/Task.java
@@ -1,14 +1,20 @@
 package com.app.YesItstodo.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
+@Getter
+@Setter
 public class Task {
     @Id
     private int id;
     private String description;
     private boolean done;
+
 }
