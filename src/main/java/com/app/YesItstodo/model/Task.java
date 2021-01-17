2d1c790b44e14a1c50d27ca520f5125a79cd4b63
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-17 16:46:56 +0100

    Annotation for id added

--- src/main/java/com/app/YesItstodo/model/Task.java
+++ src/main/java/com/app/YesItstodo/model/Task.java
@@ -1,11 +1,13 @@
 package com.app.YesItstodo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
+    @Id
     private int id;
     private String description;
     private boolean done;
