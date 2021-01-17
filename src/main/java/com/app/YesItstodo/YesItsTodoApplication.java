5e6d1f49e21aef0335b23af06352530fb4ab7024
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-17 15:13:34 +0100

    Initial commit

---
+++ src/main/java/com/app/YesItstodo/YesItsTodoApplication.java
@@ -0,0 +1,13 @@
+package com.app.YesItstodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesItsTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesItsTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/YesItstodo/YesItsTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.YesItstodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesItsTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
