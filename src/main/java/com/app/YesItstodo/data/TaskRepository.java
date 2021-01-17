244cc4df2133642576e95ac5989b1c0a791baf72
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-17 20:44:51 +0100

    Annotation @RepositoryRestResource added

--- src/main/java/com/app/YesItstodo/data/TaskRepository.java
+++ src/main/java/com/app/YesItstodo/data/TaskRepository.java
@@ -2,6 +2,8 @@ package com.app.YesItstodo.data;
 
 import com.app.YesItstodo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task,Integer> {
 }
