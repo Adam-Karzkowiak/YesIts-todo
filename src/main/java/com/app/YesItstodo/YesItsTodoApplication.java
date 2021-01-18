f30dd53ace67c1784c1a6445042d747ab5987e50
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-18 16:44:54 +0100

    validator method added, annotated as a spring bean

--- src/main/java/com/app/YesItstodo/YesItsTodoApplication.java
+++ src/main/java/com/app/YesItstodo/YesItsTodoApplication.java
@@ -2,8 +2,10 @@ package com.app.YesItstodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
 
 @SpringBootApplication
 public class YesItsTodoApplication implements RepositoryRestConfigurer {
@@ -12,11 +14,17 @@ public class YesItsTodoApplication implements RepositoryRestConfigurer {
 		SpringApplication.run(YesItsTodoApplication.class, args);
 	}
 
+	@Bean
+	Validator validator() {
+		return null;
+	}
+
 	@Override
 	public void configureValidatingRepositoryEventListener(final ValidatingRepositoryEventListener validatingRepositoryEventListener){
 		validatingRepositoryEventListener.addValidator("beforeCreate",validator());
 		validatingRepositoryEventListener.addValidator("beforeSave",validator());
 	}
+
 
 
 }
