6ab331c31b9d5892003d1f9134fa2f75ff514ecc
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-18 16:42:57 +0100

    configuration for validating repository event listener added

--- src/main/java/com/app/YesItstodo/YesItsTodoApplication.java
+++ src/main/java/com/app/YesItstodo/YesItsTodoApplication.java
@@ -2,12 +2,21 @@ package com.app.YesItstodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @SpringBootApplication
-public class YesItsTodoApplication {
+public class YesItsTodoApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YesItsTodoApplication.class, args);
 	}
+
+	@Override
+	public void configureValidatingRepositoryEventListener(final ValidatingRepositoryEventListener validatingRepositoryEventListener){
+		validatingRepositoryEventListener.addValidator("beforeCreate",validator());
+		validatingRepositoryEventListener.addValidator("beforeSave",validator());
+	}
+
 
 }
