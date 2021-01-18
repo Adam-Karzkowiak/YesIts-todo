d9f41709d80671b37f3904827399c69b9b9d2a55
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-18 22:12:53 +0100

    Delete methods are not exposed - RestResource exported set to false

--- src/main/java/com/app/YesItstodo/data/TaskRepository.java
+++ src/main/java/com/app/YesItstodo/data/TaskRepository.java
@@ -3,7 +3,16 @@ package com.app.YesItstodo.data;
 import com.app.YesItstodo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task,Integer> {
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Task task);
 }
