5ced2ca350f71f82a712d71bb985cefbfe06e937
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-18 16:39:16 +0100

    @NotNull added to description

--- src/main/java/com/app/YesItstodo/model/Task.java
+++ src/main/java/com/app/YesItstodo/model/Task.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
@@ -15,6 +16,7 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank(message = "Tasks description must be not null")
     private String description;
     private boolean done;
 
