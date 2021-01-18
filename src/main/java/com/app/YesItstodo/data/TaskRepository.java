d82b423f1cfcff8acf0f6030a92a295b92178a55
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-18 22:18:12 +0100

    Method to find task by "done" added. Path set to "/done"

--- src/main/java/com/app/YesItstodo/data/TaskRepository.java
+++ src/main/java/com/app/YesItstodo/data/TaskRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task,Integer> {
 
@@ -15,4 +17,7 @@ interface TaskRepository extends JpaRepository<Task,Integer> {
     @Override
     @RestResource(exported = false)
     void delete(Task task);
+
+    @RestResource(path = "done", rel = "done")
+    List<Task>findByDoneIsTrue();
 }
