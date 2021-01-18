fb121c7109d4c69750ab5564273599e9ed84f463
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-18 21:53:05 +0100

    Task repository path changed to "todos"

--- src/main/java/com/app/YesItstodo/data/TaskRepository.java
+++ src/main/java/com/app/YesItstodo/data/TaskRepository.java
@@ -4,6 +4,6 @@ import com.app.YesItstodo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "todos")
 interface TaskRepository extends JpaRepository<Task,Integer> {
 }
