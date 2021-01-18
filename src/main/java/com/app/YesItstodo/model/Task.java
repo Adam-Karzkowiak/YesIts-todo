85270edaf09bd720d43624ac49fbddac219bb4a7
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-18 16:22:33 +0100

    Generation type changed to IDENTITY

--- src/main/java/com/app/YesItstodo/model/Task.java
+++ src/main/java/com/app/YesItstodo/model/Task.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String description;
     private boolean done;
