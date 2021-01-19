db7f2024d75ae101ed3186bb0e2f1ea11ddcc3b3
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 22:37:13 +0100

    Created methods readTask and createTask. 1)readTask returns 200 if there is a task with the given id It returns 404 if it cannot find the task with the given id 2)createTask validates uploaded in request body Task, saves it in repository and returns code 201, created Task and id based URI.

--- src/main/java/com/app/YesItstodo/controller/TaskController.java
+++ src/main/java/com/app/YesItstodo/controller/TaskController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,8 +34,22 @@ class TaskController {
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
+
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id) {
+        return repository.findById(id)
+                .map(task -> ResponseEntity.ok(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task task) {
+        Task savedTask = repository.save(task);
+        return ResponseEntity.created(URI.create("/" + savedTask.getId())).body(savedTask);
+    }
+
     @PutMapping("/tasks/{id}")
-    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody Task task) {
+    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task task) {
         if (!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
@@ -41,4 +57,5 @@ class TaskController {
         repository.save(task);
         return ResponseEntity.noContent().build();
     }
+
 }
