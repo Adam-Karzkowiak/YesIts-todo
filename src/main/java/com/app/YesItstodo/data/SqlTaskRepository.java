ac2ee3a4ae261ae79ed5bf9311817f802a034984
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 21:24:53 +0100

    Added TaskRepository interface extension

--- src/main/java/com/app/YesItstodo/data/SqlTaskRepository.java
+++ src/main/java/com/app/YesItstodo/data/SqlTaskRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-interface SqlTaskRepository extends JpaRepository<Task, Integer> {
+interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
 
     List<Task> findByDone(@Param("state") boolean done);
 }
