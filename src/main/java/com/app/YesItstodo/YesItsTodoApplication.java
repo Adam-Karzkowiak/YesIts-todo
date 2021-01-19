65fe0dd5fb63f5f037c363dd199eccaa12235b67
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 21:04:56 +0100

    Implementation of RepositoryRestConfigurer deleted

--- src/main/java/com/app/YesItstodo/YesItsTodoApplication.java
+++ src/main/java/com/app/YesItstodo/YesItsTodoApplication.java
@@ -3,29 +3,21 @@ package com.app.YesItstodo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
-public class YesItsTodoApplication implements RepositoryRestConfigurer {
+public class YesItsTodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YesItsTodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YesItsTodoApplication.class, args);
+    }
 
-	@Bean
-	Validator validator() {
-		return new LocalValidatorFactoryBean();
-	}
-
-	@Override
-	public void configureValidatingRepositoryEventListener(final ValidatingRepositoryEventListener validatingRepositoryEventListener){
-		validatingRepositoryEventListener.addValidator("beforeCreate",validator());
-		validatingRepositoryEventListener.addValidator("beforeSave",validator());
-	}
-
+    @Bean
+    Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
 
 
 }
