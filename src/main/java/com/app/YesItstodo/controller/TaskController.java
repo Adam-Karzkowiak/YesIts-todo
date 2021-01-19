6af34be73bdd6e0a8f31ea65839377e9429c665c
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 19:52:27 +0100

    logger created (for creating logs from TaskController class)

--- src/main/java/com/app/YesItstodo/controller/TaskController.java
+++ src/main/java/com/app/YesItstodo/controller/TaskController.java
@@ -1,15 +1,21 @@
 package com.app.YesItstodo.controller;
 
 import com.app.YesItstodo.data.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 @RepositoryRestController
 class TaskController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
 
     @Autowired
     TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
+
+
+
 }
