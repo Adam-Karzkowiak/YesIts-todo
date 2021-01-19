6246ca0a331b85e3cd82e0aff9a8083a7187cc41
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 19:57:21 +0100

    readAllTasks method created

--- src/main/java/com/app/YesItstodo/controller/TaskController.java
+++ src/main/java/com/app/YesItstodo/controller/TaskController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
 
 @RepositoryRestController
 class TaskController {
@@ -16,6 +17,9 @@ class TaskController {
         this.repository = repository;
     }
 
-
+    ResponseEntity<?> readAllTasks() {
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
 
 }
