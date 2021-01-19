75c6e81ce5a3e808eec5f0185599a0d206bb3d5e
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 20:54:01 +0100

    Annotation from RepositoryRestResource changed to Repository

--- src/main/java/com/app/YesItstodo/data/TaskRepository.java
+++ src/main/java/com/app/YesItstodo/data/TaskRepository.java
@@ -3,12 +3,12 @@ package com.app.YesItstodo.data;
 import com.app.YesItstodo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RepositoryRestResource
+@Repository
 public interface TaskRepository extends JpaRepository<Task,Integer> {
 
     @Override
