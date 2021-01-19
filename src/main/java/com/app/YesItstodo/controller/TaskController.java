408e52adf978ffb0a9b04f0193503f0c2f8de287
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 20:01:56 +0100

    Unused imports deleted

--- src/main/java/com/app/YesItstodo/controller/TaskController.java
+++ src/main/java/com/app/YesItstodo/controller/TaskController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @RepositoryRestController
 class TaskController {
