f57f781418b665335f4c82ff81b84c0d659bbe4b
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 20:19:58 +0100

    Mapping for readAllTasks changed. Added params

--- src/main/java/com/app/YesItstodo/controller/TaskController.java
+++ src/main/java/com/app/YesItstodo/controller/TaskController.java
@@ -18,7 +18,7 @@ class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping("/tasks")
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
     ResponseEntity<?> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
