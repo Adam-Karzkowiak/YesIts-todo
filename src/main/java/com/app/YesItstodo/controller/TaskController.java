f84e4ea2ffa2aa6db62838855529235c0ab79fe5
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 19:48:42 +0100

    TaskRepository added as a class field. Created constructor with @Autowired annotation

--- src/main/java/com/app/YesItstodo/controller/TaskController.java
+++ src/main/java/com/app/YesItstodo/controller/TaskController.java
@@ -1,7 +1,15 @@
 package com.app.YesItstodo.controller;
 
+import com.app.YesItstodo.data.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 @RepositoryRestController
 class TaskController {
+    private final TaskRepository repository;
+
+    @Autowired
+    TaskController(final TaskRepository repository) {
+        this.repository = repository;
+    }
 }
