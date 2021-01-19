e654ab86f55afa4274e8be1871252f2366c7db4c
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 21:08:33 +0100

    Unused import deleted

--- src/main/java/com/app/YesItstodo/data/TaskRepository.java
+++ src/main/java/com/app/YesItstodo/data/TaskRepository.java
@@ -3,7 +3,6 @@ package com.app.YesItstodo.data;
 import com.app.YesItstodo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
