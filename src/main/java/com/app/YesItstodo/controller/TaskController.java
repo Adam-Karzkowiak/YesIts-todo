e715cc13db8b2d8b642905d5eb994debaf316f7b
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 21:25:53 +0100

    changed used repository interface to TaskRepository

--- src/main/java/com/app/YesItstodo/controller/TaskController.java
+++ src/main/java/com/app/YesItstodo/controller/TaskController.java
@@ -1,10 +1,9 @@
 package com.app.YesItstodo.controller;
 
-import com.app.YesItstodo.data.SqlTaskRepository;
+import com.app.YesItstodo.data.TaskRepository;
 import com.app.YesItstodo.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +14,10 @@ import java.util.List;
 @RestController
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
-    private final SqlTaskRepository repository;
+    private final TaskRepository repository;
 
-    @Autowired
-    TaskController(final SqlTaskRepository repository) {
+
+    TaskController(final TaskRepository repository) {
         this.repository = repository;
     }
 
