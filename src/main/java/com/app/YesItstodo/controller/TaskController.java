6f8baf0e69d965460cab2139d79a1723e368df63
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-19 20:01:27 +0100

    readAllTasks method mapping simplified (from RequestMapping to GetMapping)

--- src/main/java/com/app/YesItstodo/controller/TaskController.java
+++ src/main/java/com/app/YesItstodo/controller/TaskController.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RepositoryRestController
@@ -19,7 +19,7 @@ class TaskController {
         this.repository = repository;
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
+    @GetMapping("/tasks")
     ResponseEntity<?> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
