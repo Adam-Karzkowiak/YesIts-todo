f362d721971cfc27506374c128b70f7e5ed0e314
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-21 00:19:14 +0100

    Class access changed to public. Added @Configuration annotation

--- src/main/java/com/app/YesItstodo/TaskConfigurationProperties.java
+++ src/main/java/com/app/YesItstodo/TaskConfigurationProperties.java
@@ -2,10 +2,12 @@ package com.app.YesItstodo;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties("task")
 @Data
-class TaskConfigurationProperties {
+public class TaskConfigurationProperties {
     private boolean allowMultipleTasksFromTemplate;
 
 }
