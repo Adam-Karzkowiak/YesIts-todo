a376e39e68da58cc3b8d1a8b85f8da8e9b717736
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-21 00:03:48 +0100

    Class field added, lombok @Data annotation added

--- src/main/java/com/app/YesItstodo/TaskConfigurationProperties.java
+++ src/main/java/com/app/YesItstodo/TaskConfigurationProperties.java
@@ -1,7 +1,11 @@
 package com.app.YesItstodo;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("task")
+@Data
 class TaskConfigurationProperties {
+    private boolean allowMultipleTasksFromTemplate;
+
 }
