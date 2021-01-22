77d68faac1675a72c5739ee4427d46882c038ffe
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-22 21:19:08 +0100

    Added example of migration using Java

---
+++ src/main/java/db/migration/V2__insert_example_todo.java
@@ -0,0 +1,11 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+public class V2__insert_example_todo extends BaseJavaMigration {
+    @Override
+    public void migrate(final Context context) throws Exception {
+
+    }
+}
