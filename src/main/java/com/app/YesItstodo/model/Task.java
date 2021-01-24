bd00ab87fbf205959e2555373efb7efc12113201
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-24 20:57:57 +0100

    createdOn and updatedOn moved to BaseAuditableEntity

--- src/main/java/com/app/YesItstodo/model/Task.java
+++ src/main/java/com/app/YesItstodo/model/Task.java
@@ -15,33 +15,18 @@ import java.time.LocalDateTime;
 public class Task {
     @Id
     @GeneratedValue(generator = "incId")
-    @GenericGenerator(name = "incId",strategy = "increment")
+    @GenericGenerator(name = "incId", strategy = "increment")
     private int id;
     @NotBlank(message = "Tasks description must not be empty")
     private String description;
     private boolean done;
     private LocalDateTime deadline;
-    @Getter(AccessLevel.PRIVATE)
-    @Setter(AccessLevel.PRIVATE)
-    private LocalDateTime createdOn;
-    @Getter(AccessLevel.PRIVATE)
-    @Setter(AccessLevel.PRIVATE)
-    private LocalDateTime updatedOn;
 
-    @PrePersist
-    void prePersist(){
-        createdOn=LocalDateTime.now();
-    }
 
-    @PreUpdate
-    void preMerge(){
-        updatedOn=LocalDateTime.now();
-    }
-
-    public void updateFrom(final Task source){
-        description=source.description;
-        done=source.done;
-        deadline=source.deadline;
+    public void updateFrom(final Task source) {
+        description = source.description;
+        done = source.done;
+        deadline = source.deadline;
 
     }
 
