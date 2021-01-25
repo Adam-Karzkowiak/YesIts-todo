d4140a5cb687ae026488668677dca009afaccde3
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-25 21:28:37 +0100

    Added missing semicolon

--- src/main/resources/db/migration/V5__init_task_groups.sql
+++ src/main/resources/db/migration/V5__init_task_groups.sql
@@ -3,7 +3,7 @@ create table task_groups
     id          int primary key auto_increment,
     description varchar(100) not null,
     done        bit
-)
+);
 alter table tasks
     add column task_group_id int null;
 alter table tasks
