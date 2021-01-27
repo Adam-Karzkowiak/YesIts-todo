d9e1ee1e16e880e20bdded0c0a305b1ea8af5ea4
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-27 20:59:41 +0100

    HQL query added over our override method findAll() - to avoid N+1 problem

--- src/main/java/com/app/YesItstodo/data/taskGroup/SqlTaskGroupRepository.java
+++ src/main/java/com/app/YesItstodo/data/taskGroup/SqlTaskGroupRepository.java
@@ -2,8 +2,15 @@ package com.app.YesItstodo.data.taskGroup;
 
 import com.app.YesItstodo.model.TaskGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+
+    @Override
+    @Query("from TaskGroup group join fetch group.tasks")
+    List<TaskGroup> findAll();
 }
