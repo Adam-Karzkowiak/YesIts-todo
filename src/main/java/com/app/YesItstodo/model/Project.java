f4903f9545d7de0b3b2e5131082e61e2c3efc33d
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-27 21:14:39 +0100

    Project entity annotated with @Entity, Added class fields (id with generator and String description) String description validation added (NotBlank)

--- src/main/java/com/app/YesItstodo/model/Project.java
+++ src/main/java/com/app/YesItstodo/model/Project.java
@@ -1,4 +1,23 @@
 package com.app.YesItstodo.model;
 
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "projects")
 class Project {
+    @Id
+    @GeneratedValue(generator = "incId")
+    @GenericGenerator(name = "incId", strategy = "increment")
+    int id;
+
+    @NotBlank
+    String description;
+
 }
