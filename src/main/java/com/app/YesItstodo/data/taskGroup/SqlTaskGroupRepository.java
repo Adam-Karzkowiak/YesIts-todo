20423b149b1f9ccd8657999b28984a374ca82d1d
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-27 20:48:10 +0100

    Added annotation @Repository so that Spring can detect the class during the scan

--- src/main/java/com/app/YesItstodo/data/taskGroup/SqlTaskGroupRepository.java
+++ src/main/java/com/app/YesItstodo/data/taskGroup/SqlTaskGroupRepository.java
@@ -2,6 +2,8 @@ package com.app.YesItstodo.data.taskGroup;
 
 import com.app.YesItstodo.model.TaskGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
 }
