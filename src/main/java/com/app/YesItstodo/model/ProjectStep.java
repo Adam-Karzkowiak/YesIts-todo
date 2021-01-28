de31874452bc2d7730d138f1f908ed7adda82203
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-28 22:36:45 +0100

    Project set to private package access only, added lombok annotations @Data

--- src/main/java/com/app/YesItstodo/model/ProjectStep.java
+++ src/main/java/com/app/YesItstodo/model/ProjectStep.java
@@ -1,5 +1,6 @@
 package com.app.YesItstodo.model;
 
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -7,6 +8,8 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "project_steps")
+@Data
+@NoArgsConstructor
 public class ProjectStep {
     @Id
     @GeneratedValue(generator = "incId")
@@ -16,6 +19,9 @@ public class ProjectStep {
     @NotBlank
     private String description;
     private int daysToDeadline;
+
+    @Getter(AccessLevel.PRIVATE)
+    @Setter(AccessLevel.PRIVATE)
     @ManyToOne
     @JoinColumn(name = "project_id")
     private Project project;
