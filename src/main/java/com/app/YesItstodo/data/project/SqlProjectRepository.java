a7ee62e8a9748ac87b596d0af59b148c634d6a83
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-28 23:00:17 +0100

    Repository annotation added

--- src/main/java/com/app/YesItstodo/data/project/SqlProjectRepository.java
+++ src/main/java/com/app/YesItstodo/data/project/SqlProjectRepository.java
@@ -2,6 +2,8 @@ package com.app.YesItstodo.data.project;
 
 import com.app.YesItstodo.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface SqlProjectRepository extends ProjectRepository, JpaRepository<Project,Integer> {
 }
