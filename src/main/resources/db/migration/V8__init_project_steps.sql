2b3a90626ab5152fb485d776716f1aa10b055ccc
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-28 22:47:13 +0100

    V8 migration fixed (missing underline added)

--- src/main/resources/db/migration/V8__init_project_steps.sql
+++ src/main/resources/db/migration/V8__init_project_steps.sql
@@ -6,3 +6,7 @@ create table project_steps
     project_id       int          not null
         foreign key project_id references projects (id)
 );
+alter table task_groups
+    add column project_id int null;
+alter table task_groups
+    add foreign key (project_id) references projects (id);
