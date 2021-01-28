be95eb88e69174fc768cc44ea08be593c5ff410f
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-28 22:52:29 +0100

    V8 migration fixed-missing comma and parentheses have been added

--- src/main/resources/db/migration/V8__init_project_steps.sql
+++ src/main/resources/db/migration/V8__init_project_steps.sql
@@ -3,8 +3,8 @@ create table project_steps
     id               int primary key auto_increment,
     description      varchar(100) not null,
     days_to_deadline int          not null,
-    project_id       int          not null
-        foreign key project_id references projects (id)
+    project_id       int          not null,
+        foreign key (project_id) references projects (id)
 );
 alter table task_groups
     add column project_id int null;
