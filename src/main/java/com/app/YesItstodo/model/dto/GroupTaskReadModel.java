fd904b9ba080b97f405f081922a234fdc72e3495
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-01 21:32:06 +0100

    GroupTask read dto constructor created

--- src/main/java/com/app/YesItstodo/model/dto/GroupTaskReadModel.java
+++ src/main/java/com/app/YesItstodo/model/dto/GroupTaskReadModel.java
@@ -1,9 +1,15 @@
 package com.app.YesItstodo.model.dto;
 
+import com.app.YesItstodo.model.Task;
 import lombok.Data;
 
 @Data
 public class GroupTaskReadModel {
     private String description;
     private boolean done;
+
+    GroupTaskReadModel(Task source) {
+        description = source.getDescription();
+        done = source.isDone();
+    }
 }
