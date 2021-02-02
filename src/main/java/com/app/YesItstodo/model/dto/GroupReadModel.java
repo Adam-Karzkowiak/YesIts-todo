08658a69504c0f11e684311f002039fba8b950d7
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-02 21:41:49 +0100

    GroupReadModel constructor created. Deadline is set from the latest task in group.

--- src/main/java/com/app/YesItstodo/model/dto/GroupReadModel.java
+++ src/main/java/com/app/YesItstodo/model/dto/GroupReadModel.java
@@ -1,5 +1,7 @@
 package com.app.YesItstodo.model.dto;
 
+import com.app.YesItstodo.model.Task;
+import com.app.YesItstodo.model.TaskGroup;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,4 +12,13 @@ public class GroupReadModel {
     private String description;
     private LocalDateTime deadline;
     private Set<GroupTaskReadModel> tasks;
+
+
+    public GroupReadModel(TaskGroup source) {
+        description = source.getDescription();
+        source.getTasks().stream()
+                .map(Task::getDeadline)
+                .max(LocalDateTime::compareTo)
+                .ifPresent(date->deadline=date);
+    }
 }
