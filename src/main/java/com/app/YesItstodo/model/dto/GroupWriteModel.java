74de0ee85bc82bf0ae98ce265a931ace971911b6
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-02 22:01:51 +0100

    toGroup method created

--- src/main/java/com/app/YesItstodo/model/dto/GroupWriteModel.java
+++ src/main/java/com/app/YesItstodo/model/dto/GroupWriteModel.java
@@ -1,13 +1,23 @@
 package com.app.YesItstodo.model.dto;
 
+import com.app.YesItstodo.model.TaskGroup;
 import lombok.Data;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 public class GroupWriteModel {
     private String description;
     private Set<GroupTaskWriteModel> tasks;
 
-
+    public TaskGroup toGroup(){
+        var result = new TaskGroup();
+        result.setDescription(description);
+        result.setTasks(
+                tasks.stream()
+                        .map(GroupTaskWriteModel::toTask)
+                        .collect(Collectors.toSet()));
+    return result;
+    }
 }
