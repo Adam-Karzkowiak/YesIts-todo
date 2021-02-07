7a9331136442e3ce6f25088837a6f9ec5eea8e48
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-07 21:26:04 +0100

    readAll and create() method for Projects created

--- src/main/java/com/app/YesItstodo/service/ProjectService.java
+++ src/main/java/com/app/YesItstodo/service/ProjectService.java
@@ -3,8 +3,13 @@ package com.app.YesItstodo.service;
 import com.app.YesItstodo.configuration.TaskConfigurationProperties;
 import com.app.YesItstodo.data.project.ProjectRepository;
 import com.app.YesItstodo.data.taskGroup.TaskGroupRepository;
+import com.app.YesItstodo.model.Project;
+import com.app.YesItstodo.model.dto.group.GroupReadModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
@@ -18,5 +23,17 @@ public class ProjectService {
         this.groupRepository = groupRepository;
         this.projectRepository = projectRepository;
         this.configurationProperties = configurationProperties;
+    }
+
+    public List<Project> readAll() {
+        return projectRepository.findAll()
+                .stream()
+                .map(Project::new)
+                .collect(Collectors.toList());
+    }
+
+    public Project create(Project saveEntity) {
+        Project result = projectRepository.save(saveEntity);
+        return result;
     }
 }
