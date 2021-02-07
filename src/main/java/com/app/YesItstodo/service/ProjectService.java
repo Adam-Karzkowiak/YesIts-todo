40deab632a372d55c701d79801a775e13cb294a3
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-07 21:02:46 +0100

    ProjectService class fields created, repositories and config properties autowired in constructor

--- src/main/java/com/app/YesItstodo/service/ProjectService.java
+++ src/main/java/com/app/YesItstodo/service/ProjectService.java
@@ -1,7 +1,22 @@
 package com.app.YesItstodo.service;
 
+import com.app.YesItstodo.configuration.TaskConfigurationProperties;
+import com.app.YesItstodo.data.project.ProjectRepository;
+import com.app.YesItstodo.data.taskGroup.TaskGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectService {
+    private TaskGroupRepository groupRepository;
+    private ProjectRepository projectRepository;
+    private TaskConfigurationProperties configurationProperties;
+
+
+    @Autowired
+    ProjectService(final TaskGroupRepository groupRepository, final ProjectRepository projectRepository, final TaskConfigurationProperties configurationProperties) {
+        this.groupRepository = groupRepository;
+        this.projectRepository = projectRepository;
+        this.configurationProperties = configurationProperties;
+    }
 }
