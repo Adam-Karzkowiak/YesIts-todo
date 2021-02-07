d94b4421fbfd186cf95b2c12f11fdd39073cc4e3
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-07 21:41:47 +0100

    readAll simplified

--- src/main/java/com/app/YesItstodo/service/ProjectService.java
+++ src/main/java/com/app/YesItstodo/service/ProjectService.java
@@ -4,10 +4,13 @@ import com.app.YesItstodo.configuration.TaskConfigurationProperties;
 import com.app.YesItstodo.data.project.ProjectRepository;
 import com.app.YesItstodo.data.taskGroup.TaskGroupRepository;
 import com.app.YesItstodo.model.Project;
+import com.app.YesItstodo.model.dto.group.GroupReadModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,14 +28,19 @@ public class ProjectService {
     }
 
     public List<Project> readAll() {
-        return projectRepository.findAll()
-                .stream()
-                .map(Project::new)
-                .collect(Collectors.toList());
+        return projectRepository.findAll();
     }
 
     public Project create(Project saveEntity) {
         Project result = projectRepository.save(saveEntity);
         return result;
     }
+
+//    public GroupReadModel createGroup(int projectId, LocalDateTime deadline) {
+//        Optional<Project> result = projectRepository.findById(projectId);
+//        result.stream().map(Project::getGroups)
+//                .map(taskGroups -> groupRepository.existsByDoneIsFalseAndProject_Id(projectId));
+//        configurationProperties.
+//
+//    }
 }
