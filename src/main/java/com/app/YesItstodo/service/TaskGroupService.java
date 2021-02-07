04086472d74beb23b4986f9ac581233e4d2807ae
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-07 20:15:54 +0100

    Method to create TaskGroup. Method is using projection: GroupWriteModel to create TaskGroup. TaskGroup is saved to repository and then we return GroupReadModel projection

--- src/main/java/com/app/YesItstodo/service/TaskGroupService.java
+++ src/main/java/com/app/YesItstodo/service/TaskGroupService.java
@@ -1,6 +1,9 @@
 package com.app.YesItstodo.service;
 
 import com.app.YesItstodo.data.taskGroup.TaskGroupRepository;
+import com.app.YesItstodo.model.TaskGroup;
+import com.app.YesItstodo.model.dto.group.GroupReadModel;
+import com.app.YesItstodo.model.dto.group.GroupWriteModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,12 @@ public class TaskGroupService {
     @Autowired
     TaskGroupService(final TaskGroupRepository repository) {
         this.repository = repository;
+    }
+
+
+    public GroupReadModel createGroup(GroupWriteModel groupWriteModel) {
+        TaskGroup result =repository.save(groupWriteModel.toGroup());
+        return new GroupReadModel(result);
     }
 
 
