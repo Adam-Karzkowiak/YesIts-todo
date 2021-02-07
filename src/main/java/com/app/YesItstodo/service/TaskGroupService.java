8e8aa991946c3cb40e972181809091a897990f26
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-07 20:10:15 +0100

    Class field TaskGroupRepository added. Constructor created. TaskGroupRepository injected (Annotated as Autowired)

--- src/main/java/com/app/YesItstodo/service/TaskGroupService.java
+++ src/main/java/com/app/YesItstodo/service/TaskGroupService.java
@@ -1,7 +1,15 @@
 package com.app.YesItstodo.service;
 
+import com.app.YesItstodo.data.taskGroup.TaskGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 class TaskGroupService {
+    private TaskGroupRepository repository;
+
+    @Autowired
+    TaskGroupService(final TaskGroupRepository repository) {
+        this.repository = repository;
+    }
 }
